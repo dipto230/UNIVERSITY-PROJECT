4f3bf954a78551191733565e00b213143570024c
dipto230 <diptoroy230305@gmail.com>
2026-06-13 20:11:47 +0600

    setting up auth done

--- .history/frontend/src/app/routes/index_20260613201100.ts
+++ .history/frontend/src/app/routes/index_20260613201100.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { CategoryRoutes } from "../module/category/category.routes";
-import { AuthRoutes } from "../module/auth/auth.routes";
 
 
 const router = Router()
 
-router.use('/auth', AuthRoutes)
+router
 router.use('/categories', CategoryRoutes)
 
 export const IndexRoutes = router
